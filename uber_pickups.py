ded685ec7457a5e030157898d45e2a537c04b3e7
sba22014 <sba22014@student.cct.ie>
2025-05-24 21:54:28 +0100

    heeey

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber Pickups in NYC:The App')
+st.title('Uber Pickups in NYC:the App')
 DATE_COLUMN = 'date/time'
 
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
